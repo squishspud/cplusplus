8340cfbba3e67186f98d7c5fe05d179f586d0fea
squish mellow <37819271+squishspud@users.noreply.github.com>
2020-06-01 00:12:04 -0500

    Create leapyear.cpp

---
+++ leapyear.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int year;
+  cout << "Please input a year:";
+  cin >> year;
+
+  if (year < 1000 || year > 9999) {
+
+      std::cout << "Invalid entry.\n";
+
+  }
+  else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+
+    std::cout << year;
+    std::cout << " falls on a leap year.\n";
+
+  }
+  else {
+
+    std::cout << year;
+    std::cout << " is not a leap year.\n";
+
+  }
+  
+}
